b7e1c01a56ba6ad660a4cc8ad5394bc4214b0939
Omri <omrika@mta.ac.il>
2025-05-20 16:56:58 +0300

    Remove old project files and add new vehicle classes
    - Deleted `App.config` and `Ex03.GarageLogi.csproj`.
    - Updated `Ex03.GarageLogic.csproj` to include new source files: `Program.cs`, `Vehicle.cs`, `VehicleCreator.cs`, and `Wheel.cs`.
    - Introduced `Program` class with a `Main` method.
    - Added `Vehicle` class with properties for model name, license ID, energy left, and wheels.
    - Created `VehicleCreator` class for vehicle instantiation and supported types.
    - Implemented `Wheel` class with properties and methods for inflation.

---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class Vehicle
+    {
+        private string m_ModelName { get; set; }
+        private string m_LicenseId { get; set; }
+        private float m_EnergyLeft { get; set; }
+
+        private List<Wheel> m_Wheels = new List<Wheel>();
+    }
+}
---
+++ Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    internal class Wheel
+    {
+        private string m_ManufacturerName { get; set; }
+        private float m_CurrentAirPressure { get; set; }
+        private float m_MaxAirPressure { get; set; }
+        public Wheel(string i_ManufacturerName, float i_CurrentAirPressure, float i_MaxAirPressure)
+        {
+            m_ManufacturerName = i_ManufacturerName;
+            m_CurrentAirPressure = i_CurrentAirPressure;
+            m_MaxAirPressure = i_MaxAirPressure;
+        }
+        public void Inflate(float i_AirToAdd)
+        {
+            if (m_CurrentAirPressure + i_AirToAdd > m_MaxAirPressure)
+            {
+                throw new ArgumentException("Cannot inflate beyond max air pressure");
+            }
+            m_CurrentAirPressure += i_AirToAdd;
+        }
+    }
+}
